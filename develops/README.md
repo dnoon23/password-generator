ffcdd8029f6399dd885cff5b871ae968dc6f17d0
Dan Noonan <dnoonan23@yahoo.com>
2023-12-21 22:15:49 -0600

    made promps more clear. added README. renamed folders as placeholder names to get links to work.

---
+++ develops/README.md
@@ -0,0 +1,31 @@
+# <Password Generator>
+
+## Description
+
+
+<p>
+In this project I created a webpage to generate a password based on user prompts.  The user can decide how long they want their password to be and what kind of characters they want included in their password (i.e. upper case letters, lower case letters, numbers, and symbols).  During this challenge I learned how to use alerts and prompts to engage with the user and how to store these inputs to use in the code to reach a desired outcome.  The most difficult part of the project was getting the functions to loop themselves properly if there was and undesirable input selected.  Another part that was difficult for me was being able to store the password and display it properly on the webpage.
+</p>
+
+![Screenshot](./assets/)
+
+## Installation
+
+N/A
+
+## Usage
+
+To use the website open it in any browser and press the 'Generate Password' button.  The browser will begin to open prompts asking to specify how they would like their password.  Starting with an input field asking how long the password should be.  Next the user will receive prompts about what kind of characters should be included in the password.  If they would like the specific type of character the user should press 'OK', if not they should press "Cancel'.  The website also opens up prompts to let the user know whether or not each type of character was added.  After going through all the prompts the password will be displayed in on the webpage.
+
+## Credits
+
+Original code taken from: 
+https://git.bootcampcontent.com/
+
+## License
+
+N/A
+
+## Features
+
+This page features prompts to recieve inputs from a user and an event listener to respond to a user pressing the 'Generate Password' button.
--- develops/script.js
+++ develops/script.js
@@ -1,21 +1,20 @@
-// Assignment Code
+// Connects the button on the webpage to js
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Writes password to the #password input and displays it on the webpage
 function writePassword() {
   var password = pass1.join("");
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
-//sets up all possible characters that can be in the password
+
+//Vars that store all possible characters that can be in the password
 var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var symbols = [',', '!', ',', '"', ',', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~', "\\"];
 
-//sets up an array to store characters depending on choices by the user and an array to store the password
+//Sets up an array to store characters depending on choices by the user and an array to store the password after desired characters are chosen
 var pass = [];
 var pass1 = [];
 
@@ -25,43 +24,44 @@ var caseLower;
 var caseNum;
 var caseSymb;
 
-// Listener to generate button
+// Listener to run code when button is pushed
 generateBtn.addEventListener("click", function () {
-  //Asks desired password length and loops if the user enters an invalid number
-  var passwordLength = prompt("How long do you want your password to be? At least 8 characters and no more than 128.");
+
+  //Asks desired password length and loops if the user enters an invalid number.
+  var passwordLength = prompt("How long do you want your password to be? It needs to be at least 8 characters and no more than 128.");
   function passLength() {
     if (passwordLength < 8 || passwordLength > 128) {
-      passwordLength = prompt("Try again. At least 8 characters and no more than 128 characters.");
+      passwordLength = prompt("Try again. The password needs to be at least 8 characters and no more than 128 characters.");
       passLength();
     } else {
       alert('Your password will be ' + passwordLength + ' characters long.')
     }
   }
 
-  //Question for the user to customize password characters.  Confirms Selections and adds them to pass array.  If none are selected function repeats and promps the user again.
+  //Questions for the user to customize password characters.  Confirms selections and adds them to pass array.  If none are selected function repeats and promps the user again.  Will continue to repeat untill user chooses at least one set of characters.
   function question() {
-    caseUpper = confirm("Would you like upper case letters in your password?");
+    caseUpper = confirm("If you would like upper case letters in your password press OK. If not press Cancel.");
     if (caseUpper) {
       alert("Upper case letters added to password.");
       pass.push.apply(pass, upper);
     }else{
       alert("Password will not include upper case letters.")
     }
-    caseLower = confirm("Would you like lower case letters in your password?");
+    caseLower = confirm("If you would like lower case letters in your password press OK. If not press Cancel.");
     if (caseLower) {
       alert("Lower case letters added to password.");
       pass.push.apply(pass, lower);
     }else{
       alert("Password will not include lower case letters.")
     }
-    caseNum = confirm("Would you like numbers in your password?");
+    caseNum = confirm("If you would like numbers in your password press OK. If not press Cancel.");
     if (caseNum) {
       alert("Numbers added to password.");
       pass.push.apply(pass, number);
     }else{
       alert("Password will not include numbers.")
     }
-    caseSymb = confirm("Would you like symbols in your password?");
+    caseSymb = confirm("If you would like symbols in your password press OK. If not press Cancel.");
     if (caseSymb) {
       alert("Symbols added to password.");
       pass.push.apply(pass, symbols);
@@ -74,7 +74,7 @@ generateBtn.addEventListener("click", function () {
     }
   }
 
-  //Runes the previous functions 
+  //Runs the previous functions 
   passLength();
   question();
 
